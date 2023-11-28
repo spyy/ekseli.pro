82ff5afc9a0529959fde91eff83ef4908a9c3526
Sami Pyy <sami.pyy@gmail.com>
2023-11-28 15:03:03 +0200

    typo

--- src/Introduction.js
+++ src/Introduction.js
@@ -28,14 +28,15 @@ const Introduction = props => {
       
             <div className="d-flex align-items-center p-3 my-3 text-white bg-purple rounded shadow-sm">
                 <div className="lh-1">
-                    <h2 className="display-4 fst-italic">Excel tiedostosi puhelimessasi</h2>
+                    <h2 className="display-4 fst-italic">Excel tiedostosi mobiilisti</h2>
                 </div>
             </div>
     
             <div className="d-flex align-items-center p-3 my-3 text-white bg-purple rounded shadow-sm">
                 <div className="lh-1">
-                    <p className="lead my-3">ekseli.pro on ilmainen ja vapaasti käytettävä ohjelma, joka tarjoaa mobiili käyttöliittymän Excel/Spreadsheet tiedostoihisi.</p>
-                    <p className="lead my-3">Ohjelman kautta voit säätää jokaiselle tiedostollesi millaisen käyttöliittymän haluat.</p>
+                    <p className="lead my-3">1. Kirjaudu sisään.</p>
+                    <p className="lead my-3">2. Luo ohjelman kautta uusi Spreadsheet (Excel) tiedosto.</p>
+                    <p className="lead my-3">3. Säädä ohjelman kautta tiedostolle haluamasi käyttöliittymä (syötekentät).</p>
                 </div>
             </div>
 
@@ -47,28 +48,14 @@ const Introduction = props => {
             </div>
     
             <div className="my-3 p-3 bg-body rounded shadow-sm">
-                <h6 className="border-bottom pb-2 mb-0">Viimeisimmät päivitykset</h6>
+                <h6 className="border-bottom pb-2 mb-0">Viimeisimmät päivitykset</h6>               
                 <div className="d-flex text-muted pt-3">
-                    <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-                    <p className="pb-3 mb-0 small lh-sm border-bottom">
+                    <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#e83e8c"></rect><text x="50%" y="50%" fill="#e83e8c" dy=".3em">32x32</text></svg>                    
+                    <p className="pb-3 mb-0 small lh-sm border-bottom">                    
                     <strong className="d-block text-gray-dark">sami.pyy@gmail.com</strong>
-                    Versio 1.O
-                    </p>
-                </div>
-                <div className="d-flex text-muted pt-3">
-                    <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#e83e8c"></rect><text x="50%" y="50%" fill="#e83e8c" dy=".3em">32x32</text></svg>
-                    <p className="pb-3 mb-0 small lh-sm border-bottom">
-                    <strong className="d-block text-gray-dark">sami.pyy@gmail.com</strong>
-                    Halutessasi voin lisätä sinut testaajaksi.
-                    </p>
-                </div>
-                <div className="d-flex text-muted pt-3">
-                    <svg className="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
-                    <p className="pb-3 mb-0 small lh-sm border-bottom">
-                    <strong className="d-block text-gray-dark">sami.pyy@gmail.com</strong>
-                    Palvelu on testikäytössä.
-                    </p>
-                </div>
+                    (22.11.2023) Versio 1.0.                    
+                    </p>                    
+                </div>                
             </div>
     
             <div className="d-flex align-items-center p-3 my-3 text-white bg-purple rounded shadow-sm">
